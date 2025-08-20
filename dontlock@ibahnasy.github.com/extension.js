3d2397e70c744aa95c442d4440d7b607726b2412
Islam Bahnasy <ibahnasy@proton.me>
2025-08-20 20:24:48 +0300

    Fixed an issue where automatic screen lock in gnome settings where not restored if the extension toggle is un-checked

--- dontlock@ibahnasy.github.com/extension.js
+++ dontlock@ibahnasy.github.com/extension.js
@@ -6,38 +6,41 @@ export default class DisableLockExtension {
     constructor() {
         this._indicator = null;
         this._toggle = null;
+        this._lockSettings = null;
+        this._sessionSettings = null;
+
+        this._originalIdleDelay = null;
+        this._originalLockEnabled = null;
+
+        this._sessionChangedId = null;
+        this._lockChangedId = null;
     }
 
     enable() {
-        this._indicator = new QuickSettings.SystemIndicator();
-
-        // Settings objects
         this._lockSettings = new Gio.Settings({ schema_id: 'org.gnome.desktop.screensaver' });
         this._sessionSettings = new Gio.Settings({ schema_id: 'org.gnome.desktop.session' });
 
-        // Save user’s idle-delay so we can restore it later
+        // Save current prefs
         this._originalIdleDelay = this._sessionSettings.get_uint('idle-delay');
+        this._originalLockEnabled = this._lockSettings.get_boolean('lock-enabled');
 
-        // Always reset to OFF on startup
-        this._lockSettings.set_boolean('lock-enabled', true);
-        this._sessionSettings.set_uint('idle-delay', this._originalIdleDelay);
+        this._indicator = new QuickSettings.SystemIndicator();
 
         this._toggle = new QuickSettings.QuickToggle({
             title: 'DontLock',
             iconName: 'changes-prevent-symbolic',
             toggleMode: true,
         });
-
         this._toggle.checked = false;
 
         this._toggle.connect('notify::checked', (toggle) => {
             if (toggle.checked) {
-                // Disable lock + prevent idle
+                // Disable lock + idle
                 this._lockSettings.set_boolean('lock-enabled', false);
                 this._sessionSettings.set_uint('idle-delay', 0);
             } else {
-                // Restore defaults
-                this._lockSettings.set_boolean('lock-enabled', true);
+                // Restore saved values
+                this._lockSettings.set_boolean('lock-enabled', this._originalLockEnabled);
                 this._sessionSettings.set_uint('idle-delay', this._originalIdleDelay);
             }
         });
@@ -52,14 +55,21 @@ export default class DisableLockExtension {
             this._indicator = null;
         }
 
-        this._toggle.destroy();
-        this._toggle = null;
+        if (this._toggle) {
+            this._toggle.destroy();
+            this._toggle = null;
+        }
 
-        // Restore defaults
-        this._lockSettings.set_boolean('lock-enabled', true);
-        this._sessionSettings.set_uint('idle-delay', this._originalIdleDelay);
+        if (this._lockSettings && this._sessionSettings) {
+            // Restore to last known user prefs
+            this._lockSettings.set_boolean('lock-enabled', this._originalLockEnabled);
+            this._sessionSettings.set_uint('idle-delay', this._originalIdleDelay);
+        }
 
+        // Null out
         this._lockSettings = null;
         this._sessionSettings = null;
+        this._originalIdleDelay = null;
+        this._originalLockEnabled = null;
     }
 }
